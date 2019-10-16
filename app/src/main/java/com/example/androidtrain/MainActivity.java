0e67d0f503d56110106a612af2926c5caaf44cf1
eageleeye83 <lutukurthy@gmail.com>
2019-10-16 12:40:25 -0500

    animationset

--- app/src/main/java/com/example/androidtrain/MainActivity.java
+++ app/src/main/java/com/example/androidtrain/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         final RelativeLayout rlayout = findViewById(R.id.layout1);
         final ImageView train1 = findViewById(R.id.train1);
         final ImageView train2 = findViewById(R.id.train2);
+        final ImageView cloud1 = findViewById(R.id.cloud1);
+        final ImageView cloud2 = findViewById(R.id.cloud2);
         final ImageView man = findViewById(R.id.man);
 
         System.out.println("i am runing");
@@ -38,12 +40,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ObjectAnimator myanimation1 = new ObjectAnimator().ofFloat(train1,"x",1200,-1200);
-        myanimation1.setDuration(10000);
+        ObjectAnimator myanimation1 = new ObjectAnimator().ofFloat(train1,"x",-1200,1200);
+//      myanimation1.setDuration(10000);
         myanimation1.setRepeatCount(ObjectAnimator.INFINITE);
         myanimation1.setRepeatMode(ObjectAnimator.RESTART);
-//        AnimatorSet myanimationset
-        myanimation1.start();
+        ObjectAnimator myanimation2 = new ObjectAnimator().ofFloat(train2,"x",1000,-1000);
+        myanimation2.setRepeatCount(ObjectAnimator.INFINITE);
+        myanimation2.setRepeatMode(ObjectAnimator.RESTART);
+        ObjectAnimator myanimation3 = new ObjectAnimator().ofFloat(cloud1,"x",800,-800);
+        myanimation3.setRepeatCount(ObjectAnimator.INFINITE);
+        myanimation3.setRepeatMode(ObjectAnimator.RESTART);
+        ObjectAnimator myanimation4 = new ObjectAnimator().ofFloat(cloud2,"x",1000,-1000);
+        myanimation4.setRepeatCount(ObjectAnimator.INFINITE);
+        myanimation4.setRepeatMode(ObjectAnimator.RESTART);
+        ObjectAnimator myanimation5 = new ObjectAnimator().ofFloat(man,"x",-600,600);
+        myanimation5.setRepeatCount(ObjectAnimator.INFINITE);
+        myanimation5.setRepeatMode(ObjectAnimator.RESTART);
+        AnimatorSet myanimationset =new AnimatorSet();
+        myanimationset.playTogether(myanimation1,myanimation2,myanimation3,myanimation4,myanimation5);
+        myanimationset.setDuration(10000);
+        myanimationset.start();
 //
 
 
